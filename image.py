680104e8ef0c4a098a48ffedfbd92529e244eb62
Manuel Rovitti <65034835+manuelrovitti@users.noreply.github.com>
2024-03-05 16:24:26 +0100

    Add files via upload

--- image.py
+++ image.py
@@ -1,15 +1,13 @@
-from IPython.display import display, Javascript
-from google.colab.output import eval_js
-from base64 import b64decode
-
 import os
-import sys
-import numpy
+import numpy as np
 import cv2 as cv
 from PIL import Image, ImageOps, ImageDraw
 from scipy import ndimage
 from google.colab.patches import cv2_imshow # for image display
 import matplotlib.pyplot as plt
+from IPython.display import display, Javascript
+from google.colab.output import eval_js
+from base64 import b64decode
 
 def take_photo(filename='photo.jpg', quality=1.0):
   js = Javascript('''
@@ -48,13 +46,11 @@ def take_photo(filename='photo.jpg', quality=1.0):
   binary = b64decode(data.split(',')[1])
   with open(filename, 'wb') as f:
     f.write(binary)
-  print(filename)
   return filename
-
 
 def boxes(orig):
     img = ImageOps.grayscale(orig)
-    im = numpy.array(img)
+    im = np.array(img)
 
     # External gradient
     im = ndimage.morphological_gradient(im, (5, 5))
@@ -74,7 +70,7 @@ def boxes(orig):
     box = [] # array box
 
     for i in range(1, numcc + 1):
-        py, px = numpy.nonzero(lbl == i)
+        py, px = np.nonzero(lbl == i)
         if len(py) < min_size:
             im[lbl == i] = 0
             continue
@@ -84,10 +80,10 @@ def boxes(orig):
         # Four corners and centroid.
         box.append([
             [(xmin, ymin), (xmax, ymin), (xmax, ymax), (xmin, ymax)],
-            (numpy.mean(px), numpy.mean(py))])
+            (np.mean(px), np.mean(py))])
 
-    return im.astype(numpy.uint8) * 255, box
-
+    return im.astype(np.uint8) * 255, box
+  
 def find_table_index(box):
   areamindex = -1
   aream = 0
@@ -102,7 +98,7 @@ def find_table_index(box):
       areamindex = x
 
   return areamindex
-
+  
 def printimg(filename, box):
 
   img = cv.imread("photo.jpg")
@@ -133,112 +129,109 @@ def printimg(filename, box):
   #show and save
   cv2_imshow(resize)
   cv.imwrite(filename, resize)
-
-
-def main():
-    orig = Image.open("photo.jpg")
-    im, box = boxes(orig)
   
-    # Draw perfect rectangles and the component centroid.
-    img = Image.fromarray(im)
-    visual = img.convert('RGB')
-    draw = ImageDraw.Draw(visual)
-    for b, centroid in box:
-        draw.line(b + [b[0]], fill='yellow')
-        cx, cy = centroid
-        draw.ellipse((cx - 2, cy - 2, cx + 2, cy + 2), fill='red')
-    visual.show()
+def main():
+	orig = Image.open("photo.jpg")
+	im, box = boxes(orig)
 
-    #remove table cell
-    box.pop(find_table_index(box))
+	# Draw perfect rectangles and the component centroid.
+	img = Image.fromarray(im)
+	visual = img.convert('RGB')
+	draw = ImageDraw.Draw(visual)
+	for b, centroid in box:
+		  draw.line(b + [b[0]], fill='yellow')
+		  cx, cy = centroid
+		  draw.ellipse((cx - 2, cy - 2, cx + 2, cy + 2), fill='red')
+	visual.show()
 
-    #order by y center coordinate
-    box.sort(key= lambda x: x[1][1])
+	#remove table cell
+	box.pop(find_table_index(box))
 
-    #define a matrix
-    table = [[]]
-    row = 0
-    y_row_min = 0
-    y_row_max = 0
+	#order by y center coordinate
+	box.sort(key= lambda x: x[1][1])
 
-    table[row].append(box[0])
-    y_row_min = box[0][0][0][1]
-    y_row_max = box[0][0][2][1]
+	#define a matrix
+	table = [[]]
+	row = 0
+	y_row_min = 0
+	y_row_max = 0
 
-    print("FIRST RAW: y min: "+str(y_row_min)+" y max: "+str(y_row_max) + "\nPrimo elemento inserito\n")
+	table[row].append(box[0])
+	y_row_min = box[0][0][0][1]
+	y_row_max = box[0][0][2][1]
 
-    for x in box[1:]:
+	print("FIRST RAW: y min: "+str(y_row_min)+" y max: "+str(y_row_max) + "\nPrimo elemento inserito\n")
 
-        print("ACTIVE #"+str(row)+" RAW: y min: "+str(y_row_min)+" y max: "+str(y_row_max))
-        print("BOX: y_center: "+str((x[0][0][1]+x[0][2][1])/2))
+	for x in box[1:]:
 
-    if ( y_row_min*0.9 < (x[0][0][1]+x[0][2][1])/2 < y_row_max*1.1):
-        table[row].append(x)
-        if(y_row_min > x[0][0][1]):
-            y_row_min = x[0][0][1]
-        if(y_row_max < x[0][2][1]):
-            y_row_max = x[0][2][1]
+		print("ACTIVE #"+str(row)+" RAW: y min: "+str(y_row_min)+" y max: "+str(y_row_max))
+		print("BOX: y_center: "+str((x[0][0][1]+x[0][2][1])/2))
 
-        print("Elemento inserito in riga "+str(row)+"\n")
+		if ( y_row_min*0.9 < (x[0][0][1]+x[0][2][1])/2 < y_row_max*1.1):
+		  table[row].append(x)
+		  if(y_row_min > x[0][0][1]):
+		    y_row_min = x[0][0][1]
+		  if(y_row_max < x[0][2][1]):
+		    y_row_max = x[0][2][1]
 
-    else:
-        table.append([])
-        row = row + 1
-        table[row].append(x)
-        y_row_min = x[0][0][1]
-        y_row_max = x[0][2][1]
+		  print("Elemento inserito in riga "+str(row)+"\n")
 
-        print("Nuova riga creata e elemento inserito in riga "+str(row)+"\n")
+		else:
+		  table.append([])
+		  row = row + 1
+		  table[row].append(x)
+		  y_row_min = x[0][0][1]
+		  y_row_max = x[0][2][1]
 
-    #sort for x coordinate (sort one raw at time)
-    for x in table:
-        x.sort(key= lambda x: x[1][0])
+		  print("Nuova riga creata e elemento inserito in riga "+str(row)+"\n")
 
-    w=len(table)
-    h=len(table[0])
+	#sort for x coordinate (sort one raw at time)
+	for x in table:
+		x.sort(key= lambda x: x[1][0])
 
-    print("La tabella è di dimensione: "+str(w)+"x"+str(h)+"\n")
+	w=len(table)
+	h=len(table[0])
 
+	print("La tabella è di dimensione: "+str(w)+"x"+str(h)+"\n")
 
-    #save image
-    final_arr=[]
-    for row in range(len(table)):
-        for col in range(len(table[row])):
-            filename = str(row)+str(col)+".jpg"
-            print(filename)
-            print(table[row][col])
-            printimg(filename, table[row][col])
-            final_arr.append(cv.imread(filename))
+	#save image
+	final_arr=[]
+	for row in range(len(table)):
+		for col in range(len(table[row])):
+		  filename = str(row)+str(col)+".jpg"
+		  print(filename)
+		  print(table[row][col])
+		  printimg(filename, table[row][col])
+		  final_arr.append(cv.imread(filename))
+  
+	#final print
+	for i in range(len(box)):
+		plt.subplot(w,h,i+1)
+		plt.xticks([])
+		plt.yticks([])
+		plt.grid(False)
+		plt.imshow(final_arr[i])
+		#plt.xlabel(class_names[label])
+	plt.show()
 
-    #final print
-    for i in range(len(box)):
-        plt.subplot(w,h,i+1)
-        plt.xticks([])
-        plt.yticks([])
-        plt.grid(False)
-        plt.imshow(final_arr[i])
-        #plt.xlabel(class_names[label])
-    
-    plt.show()
+	if os.path.isfile("photo.jpg"):
+		os.remove("photo.jpg")
 
-    if os.path.isfile("photo.jpg"):
-        os.remove("photo.jpg")
+	# Check if directory "output" exists and remove it recursively
+	if os.path.exists("output"):
+		for filename in os.listdir("output"):
+			file_path = os.path.join("output", filename)
+			try:
+				if os.path.isfile(file_path) or os.path.islink(file_path):
+					os.unlink(file_path)
+				elif os.path.isdir(file_path):
+					os.rmdir(file_path)
+			except Exception as e:
+				print(f"Failed to delete {file_path}. Reason: {e}")
+		os.rmdir("output")
 
-    # Check if directory "output" exists and remove it recursively
-    if os.path.exists("output"):
-        for filename in os.listdir("output"):
-            file_path = os.path.join("output", filename)
-            try:
-                if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path):
-                    os.rmdir(file_path)
-            except Exception as e:
-                print(f"Failed to delete {file_path}. Reason: {e}")
-        os.rmdir("output")
+	os.makedirs("output")
 
-    os.makedirs("output")
-
-    for filename in os.listdir():
-        if filename.endswith(".jpg"):
-            os.rename(filename, os.path.join("output", filename))
+	for filename in os.listdir():
+		if filename.endswith(".jpg"):
+			os.rename(filename, os.path.join("output", filename))
